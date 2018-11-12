4ef9443625e091a26f32013da6a194c8674e3fbc
neskaSpahic <nesibaspahic1997@gmail.com>
2018-11-13 00:12:16 +0100

    "post"

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,9 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable} from 'rxjs';
 import { Post } from '../models/Post';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class PostService {
   postsUrl: string = 'https://jsonplaceholder.typicode.com/posts';
 
@@ -17,4 +23,10 @@ export class PostService {
    {
       return this.http.get<Post[]>(this.postsUrl);
    }
+
+   savePost(post: Post): Observable<Post>{
+     return this.http.post<Post>(this.postsUrl,post,httpOptions);
+   }
+
+
 }
