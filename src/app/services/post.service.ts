0ab8948cbed196f897e951575f87532be75a6ee8
neskaSpahic <nesibaspahic1997@gmail.com>
2018-11-13 00:14:01 +0100

    "post"

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable} from 'rxjs';
 import { Post } from '../models/Post';
-
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 }
+
 
 @Injectable({
   providedIn: 'root'
